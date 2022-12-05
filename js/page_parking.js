35f4fda9e417fff51038f2a9e6de77f79e0703f4
Theo Seuge <theoseuge@neey.local>
2022-12-05 15:17:47 +0100

    page parking

--- js/page_parking.js
+++ js/page_parking.js
@@ -1,7 +1,7 @@
 //intialisation des variables de coordonnées
 var latitude = 0;
 var longitude = 0;
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById("myChart");
 
 //utilisation de fetch pour récup les données de l'API
 fetch(
@@ -16,13 +16,10 @@ fetch(
   //nouvelle fonction pour traiter les data recupérées
   .then(function getAdress(data) {
     //initialisation des variables de stockage des données
-    var parkingName = "";
-    var capaciteMax = "";
-    var placeDispo = "";
-
     let parking = [];
     let dispo = [];
     let max = [];
+    let location = []
 
     //boucle pour parcourir les données
     for (let i = 0; i < data.records.length; i++) {
@@ -30,70 +27,54 @@ fetch(
       parking.push(data.records[i].record.fields.grp_nom);
       dispo.push(data.records[i].record.fields.disponibilite);
       max.push(data.records[i].record.fields.grp_exploitation);
+      location.push(data.records[i].record.fields.location);
+
     }
 
-    console.log(parking);
-    console.log(dispo);
-    console.log(max);
-
-    //pour le nom du parking
-    parkingName = data.records[0].record.fields.grp_nom;
-
-    //recup la capacité maximale
-    capaciteMax = data.records[0].record.fields.grp_exploitation;
-    //recup le nombre de places dispo
-    placeDispo = data.records[0].record.fields.disponibilite;
-
-    placeOccuped = capaciteMax - placeDispo
-
-    //les données sont injectées dans le html
-    document.getElementById("nom_parking").innerHTML = parkingName;
-    document.getElementById("capacite_max").innerHTML =
-      "Capacité maximale : " + capaciteMax;
-    document.getElementById("places_dispo").innerHTML =
-      "Places disponibles : " + placeDispo;
-    
-      new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: ['Places vide', 'Places occupées'],
-          datasets: [{
-            label: '',
-            data: [placeDispo, placeOccuped],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
-
-    
-  //recup les coordonnées GPS du parking
-    var parkingAdress = "";
-    parkingAdress = data.records[0].record.fields.location;
-    //je separe les valeurs du tableau en deux 
-    latitude = parkingAdress.lat;
-    longitude = parkingAdress.lon;
-    //paramètres de la petite map pour l'adresse du parking
-    var map = L.map("map").setView([latitude, longitude], 16);
-    var marker = L.marker([latitude, longitude]).addTo(map) ;
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
-
-  
-    console.log(parkingName);
-    console.log(parkingAdress);
-    console.log(capaciteMax);
-    console.log(placeOccuped);
-    console.log(placeDispo);
+    //Feydeau
+    for (let i = 0; i < parking.length; i++) {
+      if (parking[i] == "Feydeau") {
+        document.getElementById("capacite_max").innerHTML =
+          "Capacité maximale : " + max[i];
+        document.getElementById("places_dispo").innerHTML =
+          "Places disponibles : " + dispo[i];
+        parkingAdress = location
+        console.log(parkingAdress[i])
+        //je separe les valeurs du tableau en deux
+        let latitude = parkingAdress[i].lat;
+        let longitude = parkingAdress[i].lon
+        var map = L.map("map").setView([latitude, longitude], 16);
+        var marker = L.marker([latitude, longitude]).addTo(map);
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 19,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(map);
+        new Chart(ctx, {
+          type: "pie",
+          data: {
+            labels: ["Places vide", "Places occupées"],
+            datasets: [
+              {
+                label: "",
+                data: [max[i], dispo[i]],
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        });
+      }
+      console.log(parking[i])
+      console.log(max[i])
+      console.log(dispo[i])
+    }
   });
 
 //le fetch s'actualise toutes les 2 minutes
